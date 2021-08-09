85358b34941a4913cd5d205472923e1d3a570f1a
Ioana Chiorean <73991240+ioanachiorean@users.noreply.github.com>
2021-08-09 23:34:00 +0300

    created signup-form
    used reactive forms from ang lesson

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
+
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FavButtonComponent } from './fav-button/fav-button.component';
+import { SignupFormComponent } from './signup-form/signup-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FavButtonComponent
+    FavButtonComponent,
+    SignupFormComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/signup-form/signup-form.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'signup-form',
+  templateUrl: './signup-form.component.html',
+  styleUrls: ['./signup-form.component.css']
+})
+export class SignupFormComponent implements OnInit {
+  form = new FormGroup({
+    username: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3)
+    ]),
+    password: new FormControl('', Validators.required)
+  })
+
+  get username()
+{
+    return this.form.get('username');
+}
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+
+}
