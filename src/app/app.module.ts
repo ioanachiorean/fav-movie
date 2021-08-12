9d549ffa9bd8059f8cba6f4c3d4eee064605aec7
Ioana Chiorean <73991240+ioanachiorean@users.noreply.github.com>
2021-08-12 11:23:47 +0300

    created new-course-component
    input field with class="form-control" that creates list of items that can be deleted on click. cool stuff, to be remembered in the future

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { AppComponent } from './app.component';
 import { FavButtonComponent } from './fav-button/fav-button.component';
 import { SignupFormComponent } from './signup-form/signup-form.component';
 import { NavPillsComponent } from './nav-pills/nav-pills.component';
+import { NewCourseFormComponent } from './new-course-form/new-course-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FavButtonComponent,
     SignupFormComponent,
-    NavPillsComponent
+    NavPillsComponent,
+    NewCourseFormComponent
 
   ],
   imports: [
---
+++ src/app/new-course-form/new-course-form.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
+
+
+@Component({
+  selector: 'new-course-form',
+  templateUrl: './new-course-form.component.html',
+  styleUrls: ['./new-course-form.component.css']
+})
+export class NewCourseFormComponent implements OnInit {
+  form = new FormGroup({
+    topics: new FormArray([])
+  });
+
+  addTopic(topic: HTMLInputElement){
+    this.topics.push(new FormControl(topic.value));
+    topic.value = ' ';
+
+  }
+
+    
+    public get topics() {
+      return this.form.get('topics') as FormArray;
+    }
+     
+
+  removeTopic(topic: FormControl) {
+    let index = this.topics.controls.indexOf(topic);
+    this.topics.removeAt(index);
+
+  }
+  
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
+
+
+
